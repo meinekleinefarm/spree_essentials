f965a7b02cdf6acf1da8a4c3ae95c10c1c59f280
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-08-28 16:59:10 +0200

    Manually fix content submenu

--- app/overrides/admin/spree_essentials.rb
+++ app/overrides/admin/spree_essentials.rb
@@ -4,10 +4,3 @@ Deface::Override.new(:virtual_path  => "spree/layouts/admin",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :partial       => "spree/admin/shared/contents_tab",
                      :disabled      => false)
-
-# Adds the contents menu. Inside the contents sub menu we ensure it's needed
-Deface::Override.new(:virtual_path  => "spree/layouts/admin",
-                     :name          => "spree_essential_contents_menu",
-                     :insert_top    => "#sub-menu[data-hook]",
-                     :partial       => "spree/admin/shared/contents_sub_menu",
-                     :disabled      => false)
